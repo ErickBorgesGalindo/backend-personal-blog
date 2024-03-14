be9b879508f169c50648da07988726ea3e0e5d04
Alecs <erick.borges@outlook.es>
2024-03-14 15:58:55 -0600

    feature: enviroment, cors

--- src/db.js
+++ src/db.js
@@ -1,11 +1,12 @@
 const { Pool } = require('pg');
+const {db} = require('./config');
 
 const pool = new Pool({
-    user: 'user_name',
-    password: 'pass',
-    host: 'localhost',
-    port: 5432,
-    database: 'data_name'
+    user: db.user,
+    password: db.password,
+    host: db.host,
+    port: db.port,
+    database: db.database
 });
 
 module.exports = pool;  
